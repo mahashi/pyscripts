6dd8dc30fa25d63ebafdd4cbd1bcda7171aa8613
Mohamed Hashi <mhas@spreadshirt.net>
2016-12-19 14:51:25 +0100

    Wrapped script in a function accept command-line arguments

--- sizeConverter.py
+++ sizeConverter.py
@@ -1,15 +1,22 @@
 from xml.dom.minidom import parse
-import os
+from os import walk
 
-for root, dirs, files in os.walk("~/Code/rAppid.js-sprd/sprd/img/masks"):
-    for file in files:
-        if file.endswith('.svg'):
-            print(file)
-            f = open(file)
-            dom = parse(f)
-            f.close()
-            f = open(file, 'w')
-            dom.documentElement.setAttribute('width', '100')
-            dom.documentElement.setAttribute('height', '100')
-            dom.documentElement.writexml(f)
-            f.close()
+def changeWidthAndHeight(files):
+    print(file)
+    f = open(file)
+    dom = parse(f)
+    f.close()
+    f = open(file, 'w')
+    dom.documentElement.setAttribute('width', '100')
+    dom.documentElement.setAttribute('height', '100')
+    dom.documentElement.writexml(f)
+    f.close()
+
+def getSvgFiles(dir):
+    svgs = []
+    for _, _, files in walk(dir):
+        for file in files:
+            if file.endswith('.svg'):
+                svgs[] = file;
+
+    return svgs
